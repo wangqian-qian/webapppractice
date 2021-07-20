72e3da5ec76bd1a383cd443b62cdc018bdf3fa90
wangqian-qian <wangqian118@163.com>
2021-07-20 09:41:19 +0800

    second commit

---
+++ src/main/java/com/example/webappratice/WebappraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webappratice/WebappraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
